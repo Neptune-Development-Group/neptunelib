25ba8b73d8d4900d0925c5866345fbbcecb9a8be
Creativious <timothyu120@gmail.com>
2023-12-31 12:51:28 -0500

    working on extra World functions

---
+++ src/main/java/com/neptunedevelopmentteam/neptunelib/interfaces/NeptuneWorld.java
@@ -0,0 +1,13 @@
+package com.neptunedevelopmentteam.neptunelib.interfaces;
+
+import net.minecraft.util.math.BlockPos;
+
+public interface NeptuneWorld {
+
+    /**
+     * Updates the block if it is on the server.
+     *
+     * @param  blockPos  the position of the block to update
+     */
+    void updateBlockIfOnServer(BlockPos blockPos);
+}
